616102483610ad9d07582c77f18ade8bbb407e8a
denrique-alvarez <denrique.alvarez@gmail.com>
2022-05-16 12:42:42 +0200

    correction database setup

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('postgres://xmorqzoqfjntwl:e25e59d300ab7d75f49dbc96d850606095dbf6ae2123be09870eedeb9a88f3ad@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7asl7i4n8g41o'),
+  url: env('MY_HEROKU_URL'),
   app: { 
     keys: env.array('APP_KEYS')
   },
